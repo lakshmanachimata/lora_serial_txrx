085a2a64ab10c7e9021a9d9e6d53d841d3afff4e
lakshmana <lakshman.chimata@gmail.com>
2017-09-21 18:43:52 +0530

    added new code

---
+++ serial_tx_rx.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import Tkinter, tkMessageBox
+import serial
+from Tkinter import *
+from tkMessageBox import *
+import binascii
+
+global sendData
+global recvData
